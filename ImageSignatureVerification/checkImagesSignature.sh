2221b6b30450bf76e85b09ae0ac8545c29d73135
Alexey Kostarev <kaf@altlinux.org>
2023-03-01 13:15:44 +0500

    Tune checkImagesSignature.sh - JSON output

--- ImageSignatureVerification/checkImagesSignature.sh
+++ ImageSignatureVerification/checkImagesSignature.sh
@@ -1,76 +1,120 @@
 #!/bin/sh
 
+export mes="\"host\":\"$HOSTNAME\""
 defaultPolicy=$(jq '.default[].type' /etc/containers/policy.json  | tr -d '"')
 if [ $defaultPolicy != 'reject' ]
 then
-  echo "В /etc/containers/policy.json установлена политика по умолчанию '$defaultPolicy' отличная от reject"
-  exit 1
+  mes="${mes}\n\"incorrectDefaultPolicy\":\"$defaultPolicy\""
+#   echo "В /etc/containers/policy.json установлена политика по умолчанию '$defaultPolicy' отличная от reject"
+  echo -ne "{$mes}" | tr "\n" ',' | jq .
+  #exit 1
 fi
 
 notSigned=$(jq '.transports.docker  | to_entries | .[]  | if .value[].type != "signedBy" then .key else empty end' /etc/containers/policy.json)
 if [ -n "$notSigned" ]
 then
-  echo "Регистраторы не использующие механизм подписи в  /etc/containers/policy.json:"
-  echo $notSigned | tr ' ' "\n"
-  echo
+#   echo "Регистраторы не использующие механизм подписи в  /etc/containers/policy.json:"
+  notSignedRegistry=$(echo $notSigned | tr ' ' "\n")
+#   echo -ne "$notSignedRegistry\n"
+  mes="${mes}\n\"notSignedRegistry\":[$notSignedRegistry]"
 #   exit 1
 fi
 
-registries=$(jq '.transports.docker | keys | .[]' /etc/containers/policy.json | tr -d '"')
-n=0
-for registry in $registries
-do
-#   echo $registry
-  if [ $n -eq 0 ]
-  then
-    in="    (. | startswith(\"$registry/\"))\n"
-    out="    (. | startswith(\"$registry/\") | not)\n"
-  else
-    in="$in    or \n    (. | startswith(\"$registry/\"))\n"
-    out="$out    and \n    (. | startswith(\"$registry/\") | not)\n"
-  fi
-  let n=$n+1
-done
 
-in="podman images --format json |
-jq '[.[].Names | .[]] |
+export registries=$(jq '.transports.docker | keys | .[]' /etc/containers/policy.json | tr -d '"')
+
+checkUserImages() {
+  user=$1
+  mes="${mes}\n\"$user\": {\"user\": \"$user\""
+  n=0
+  for registry in $registries
+  do
+  #   echo $registry
+    if [ $n -eq 0 ]
+    then
+      in="    (. | startswith(\"$registry/\"))\n"
+      out="    (. | startswith(\"$registry/\") | not)\n"
+    else
+      in="$in    or \n    (. | startswith(\"$registry/\"))\n"
+      out="$out    and \n    (. | startswith(\"$registry/\") | not)\n"
+    fi
+    let n=$n+1
+  done
+
+  in="su - -c 'podman images --format json' $user 2>/dev/null |
+  jq '[.[] | if  has(\"Names\") then .Names else [.Id] end | .[]] |
 map(
   select(
 $in  )
 ) | .[]'
 "
 
-out="podman images --format json |
- jq '[.[].Names | .[]] |
+  out="su - -c 'podman images --format json' $user 2>/dev/null |
+  jq '[.[] | if  has(\"Names\") then .Names else [.Id] end | .[]] |
 map(
   select(
 $out  )
 ) | .[]'
 "
 
-TMPFILE="/tmp/checkImagesSignature.$$"
-echo -ne "$in" > $TMPFILE
-in=$(sh $TMPFILE | tr -d '"')
+  TMPFILE="/tmp/checkImagesSignature.$$"
+  echo -ne "$in" > $TMPFILE
+  in=$(sh $TMPFILE )
 
-echo -ne "$out" > $TMPFILE
-out=$(sh $TMPFILE | grep -v "localhost/podman-pause" | tr -d '"')
-rm -f $TMPFILE
+  echo -ne "$out" > $TMPFILE
+  out=$(sh $TMPFILE | grep -v "localhost/podman-pause" )
+  rm -f $TMPFILE
 
-if [ -n "$out" ]
-then
-  echo "Образы вне политики файла /etc/containers/policy.json:"
-  echo $out | tr ' ' "\n"
-  echo
-fi
+  TMPFILE="${TMPFILE}--"
+  if [ -n "$out" ]
+  then
+#     echo "Рабочее местр $HOSTNAME. пользователь $user: Образы вне политики файла /etc/containers/policy.json:"
+    outPolicyImages=$(echo $out | tr ' ' "\n")
+#     echo "$outPolicyImages\n"
+    mes="${mes}\n\"outPolicyImages\": [$outPolicyImages]"
+  fi
 
-if [ -n "$out" ]
-then
-  echo "Образы согласно политики файла /etc/containers/policy.json:"
-  echo $in | tr ' ' "\n"
-  echo
-fi
+  if [ -n "$in" ]
+  then
+#     echo "Рабочее местр $HOSTNAME. пользователь $user: Образы согласно политики файла /etc/containers/policy.json:"
+    inPolicyImages=$(echo $in | tr ' ' "\n")
+#     echo "$inPolicyImages\n"
+    mes="${mes}\n\"inPolicyImages\": [$inPolicyImages]"
+  fi
+  inCorrectImages=
+  for image in $in
+  do
+    Image=$(echo $image | tr -d '"')
+#     echo -ne "Проверка образа $Image... "
+    if podman pull --tls-verify=false $Image >/dev/null 2>$TMPFILE
+    then :; # echo "OK";
+    else
+#       echo "Рабочее местр $HOSTNAME. пользователь $user имеет некорректный образ:"
+#       cat $TMPFILE
+      inCorrectImages="{$image:\"$(tr '"' '\"' < $TMPFILE)\"}"
+    fi
+  done
+  mes="${mes},\"inCorrectImages\":[$inCorrectImages]"
+  mes="${mes}}"
+  rm -f $TMPFILE
+}
 
-for image in $in
+#checkUserImages root
+# user=kaf
+# checkImages $user
+# exit
+
+for user in /home/*
 do
-  podman pull --tls-verify=false $image
+  if [ -d "$user/.local/share/containers/storage/overlay" ]
+  then
+    user=$(basename $user)
+#     echo -ne "\n\n------------------------$user\n"
+    checkUserImages $user
+  fi
 done
+mes=$(echo -ne "{$mes}" | tr "\n" ',')
+# echo -ne "MES=$mes\n"
+echo -ne "$mes"
+# | jq .
+
