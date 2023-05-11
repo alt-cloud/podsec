1db8abc87fb2d2c85f647e28daafdeb97c5204a9
Alexey Kostarev <kaf@altlinux.org>
2023-05-11 11:37:25 +0500

    Настрока сети

--- usernetes/bin/_kubelet.sh
+++ usernetes/bin/_kubelet.sh
@@ -7,7 +7,7 @@ logger -- "`(echo -ne "$0: TIME=$(date  +%H:%M:%S.%N) UID=$UID PID=$(cat $XDG_RU
 echo -ne "$0: TIME=$(date  +%H:%M:%S.%N) UID=$UID PID=$(cat $XDG_RUNTIME_DIR/usernetes/rootlesskit/child_pid) PARS=$*\n"  >&2
 
 # Обеспечить доступ с cni0 интерфеса сети 10.244.0.1/24  доступ к внешним и внутранним анонсированным адресам (порты tcp/6443, udp/53, ...)
-iptables -t nat -I POSTROUTING -s 10.244.0.1/24  -j SNAT --to $U7S_TAPIP
+# iptables -t nat -I POSTROUTING -s 10.244.0.1/24  -j SNAT --to $U7S_TAPIP
 # Обеспечить доступ к порту 10.96.0.10:53 через порт 6053
 # iptables -t nat -I PREROUTING -p tcp -d 0.0.0.0/0 --dport 6053 -j DNAT --to=10.96.0.10:53
 # iptables -t nat -I PREROUTING -p udp -d 0.0.0.0/0 --dport 6053 -j DNAT --to=10.96.0.10:53
