372cc6577919e224d12c4bb9130074fe06ffc6f1
Alexey Kostarev <kaf@altlinux.org>
2023-05-13 07:33:36 +0500

    Находим правильный каталог для etcd

--- usernetes/bin/_kubeadm.sh
+++ usernetes/bin/_kubeadm.sh
@@ -9,12 +9,27 @@ echo -ne "$0: TIME=$(date  +%H:%M:%S.%N) UID=$UID PID=$(cat $XDG_RUNTIME_DIR/use
 # extIP=$1
 cmd=$1
 
-if [ "$U7S_CONTROLPLANE" = 'initMaster' ]
-then
-  # Создаем каталог базы etcd
-  rm -rf /var/lib/Etcd
-  mkdir -p /var/lib/Etcd
-fi
+# if [ "$U7S_CONTROLPLANE" = 'initMaster' ]
+# then
+#   # Создаем каталог базы etcd
+#   rm -rf /var/lib/Etcd
+#   mkdir -p /var/lib/Etcd
+# fi
+
+# if [ "$U7S_CONTROLPLANE" = 'initMaster' ]
+# then
+#   # Создаем каталог базы etcd
+#   rm -rf /var/lib/u7s-admin/usernetes/var/lib/etcd
+#   mkdir -p /var/lib/u7s-admin/usernetes/var/lib/etcd
+# fi
+#
+# if [ "$U7S_CONTROLPLANE" = 'initMaster' ]
+# then
+#   # Создаем каталог базы etcd
+#   rm -rf /var/lib/podsec/etcd
+#   mkdir -p /var/lib/podsec/etcd
+#   chown u7s-admin:u7s-admin /var/lib/podsec/etcd
+# fi
 
 # Копируем coredns.yaml  kube-flannel.yml
 # cp /etc/podsec/u7s/config/manifests/* /etc/kubernetes/manifests/
