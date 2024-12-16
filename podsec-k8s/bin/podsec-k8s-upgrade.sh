a8d8a5a4bf4f899b315cdbeed432fe48dd95c83c
Alexey Kostarev <kaf@altlinux.org>
2024-12-16 15:13:51 +0500

    Added support upgrade to 1.30

--- podsec-k8s/bin/podsec-k8s-upgrade.sh
+++ podsec-k8s/bin/podsec-k8s-upgrade.sh
@@ -3,6 +3,17 @@
 export TEXTDOMAINDIR='/usr/share/locale'
 export TEXTDOMAIN='podsec-k8s'
 
+function getCurrentFlannelVersion() {
+  ret=''
+  flannelImage=$(kubectl -n kube-flannel get daemonset.apps/kube-flannel-ds -o yaml |
+    yq '.spec.template.spec.containers[0].image')
+  if [ -n "$flannelImage" ]
+  then
+    flannelImage=$(basename $flannelImage)
+    ifs=$IFS; IFS=:; set -- $flannelImage; IFS=$ifs; ret=${2:1}
+  fi
+  echo -ne $ret
+}
 
 function getNodeCurrentKubeletVersion() {
   ret=''
@@ -53,32 +64,51 @@ function getNextKubeVersions() {
   else
     protocol='https'
   fi
-  kubeVersions=$(curl -sk $protocol://$U7S_REGISTRY/v2/${U7S_PLATFORM}/kube-apiserver/tags/list | jq -r .tags[] | sort)
+  kubeVersions=$(curl -sk $protocol://$U7S_REGISTRY/v2/${U7S_PLATFORM}/kube-apiserver/tags/list | jq -r .tags[] | sort -V)
   ret=''
+  lastMinorKubeVersion=$curMinorKubeVersion
+  lastPatchKubeVersion=''
   for kubeVersion in $kubeVersions
   do
+    if [ "${kubeVersion:0:1}" != 'v' ]; then continue; fi
     kubeVersion=${kubeVersion:1}
-    if [[ "$(getMinorVersion $kubeVersion)" > "$curMinorKubeVersion" ]]
+    curMinor=$(getMinorVersion $kubeVersion)
+    if [[ "$curMinor" > "$curMinorKubeVersion" ]]
     then
-      ret+=" $kubeVersion"
+      if [ -n "$lastPatchKubeVersion" -a "$curMinor" != "$lastMinorKubeVersion" ]
+      then
+        ret+="$lastPatchKubeVersion\n"
+      fi
+    else
+      continue
     fi
-    if [ -n "${toMinorVersion}" -a "${kubeVersion:0:${#toMinorVersion}}" = "${toMinorVersion}" ]
+    if [ -n "${toMinorVersion}" ]
     then
-      break
+      if [[ "${kubeVersion:0:${#toMinorVersion}}" > "${toMinorVersion}" ]]
+      then
+        break
+      fi
     fi
+    lastPatchKubeVersion=$kubeVersion
+    lastMinorKubeVersion=$curMinor
   done
+  ret+="$lastPatchKubeVersion\n"
+  ret="$(echo -ne "$ret" | sort -u)"
   echo $ret
 }
 
 function setRPMRegistries() {
   if [ "$U7S_REGISTRY" = 'registry.local' ]
   then
-    export U7S_RPMRegistryClassic='http://sigstore.local:81/rpms'
-    export U7S_RPMRegistryNoarch='http://sigstore.local:81/rpms'
+    apt-repo add 'rpm http://sigstore.local:81/kubernetes_upgrade x86_64 main'
+    export U7S_RPMRegistryClassic='http://sigstore.local:81/kubernetes_upgrade/x86_64/RPMS.main/'
+    export U7S_RPMRegistryNoarch='http://sigstore.local:81/kubernetes_upgrade/x86_64/RPMS.main/'
   elif [ "${U7S_PLATFORM:0:8}" = 'k8s-c10f' ]
   then
-    export U7S_RPMRegistryClassic='http://update.altsp.su/${U7S_PLATFORM}/branch/x86_64/RPMS.classic'
-    export U7S_RPMRegistryNoarch='http://update.altsp.su/${U7S_PLATFORM}/branch/noarch/RPMS.classic'
+    apt-repo add 'rpm [cert8] http://update.altsp.su/pub/distributions/ALTLinux c10f/branch/x86_64-i586 classic'
+    apt-repo add 'rpm [cert8] http://update.altsp.su/pub/distributions/ALTLinux c10f/branch/noarch classic'
+    export U7S_RPMRegistryClassic="http://update.altsp.su/${U7S_PLATFORM:4}/branch/x86_64/RPMS.classic"
+    export U7S_RPMRegistryNoarch="http://update.altsp.su/${U7S_PLATFORM:4}/branch/noarch/RPMS.classic"
   fi
 }
 
@@ -105,9 +135,11 @@ function loadRpmPackages() {
     then
       listClassicRPMS+=' '$(jq -r '.[] | select(.name[0:15]=="kubernetes'$kubeMinorVersion'-") | .name' $U7S_rpmDir/classicPkgs)
       listClassicRPMS+=' '$(jq -r '.[] | select(.name[0:10]=="cri-o'$kubeMinorVersion'-") | .name' $U7S_rpmDir/classicPkgs)
+      listClassicRPMS+=' '$(jq -r '.[] | select(.name[0:10]=="cri-tools'$kubeMinorVersion'-") | .name' $U7S_rpmDir/classicPkgs)
     else
       listClassicRPMS+=' '$(grep kubernetes${kubeMinorVersion} $U7S_rpmDir/classicPkgs | sed -e 's|</a>.*||' -e 's|<a href=.*>||')
       listClassicRPMS+=' '$(grep cri-o${kubeMinorVersion} $U7S_rpmDir/classicPkgs | sed -e 's|</a>.*||' -e 's|<a href=.*>||')
+      listClassicRPMS+=' '$(grep cri-tools${kubeMinorVersion} $U7S_rpmDir/classicPkgs | sed -e 's|</a>.*||' -e 's|<a href=.*>||')
     fi
 
     if [ "$U7S_RPMRegistryClassic" != "$U7S_RPMRegistryNoarch" ]
@@ -163,12 +195,13 @@ then
   exit 1
 fi
 
-export prevImages=$(machinectl shell u7s-admin@ /usr/bin/kubeadm config images list --image-repository=$U7S_REGISTRY 2>/dev/null)
+export prevImages=$(machinectl shell u7s-admin@ /usr/bin/kubeadm config images list --image-repository=$U7S_REGISTRY | grep $U7S_REGISTRY | tr -d '\r')
 echo "kubeVersion $kubeVersion
 kubeMinorVersion=$kubeMinorVersion
 nextKubeVersions=$nextKubeVersions
 prevImages=$prevImages
 "
+# exit
 
 if [ -z "$nextKubeVersions" ]
 then
@@ -222,33 +255,94 @@ do
         --image-repository=$U7S_REGISTRYPATH \
         --kubernetes-version=v${kubeVersion}
 
+  if [[ "$kubeMinorVersion" > '1.26' ]]
+  then
+    currentFlannelVersion=$(getCurrentFlannelVersion)
+    toFlannelVersion='1.25.1'
+    if [ "$currentFlannelVersion" != "$toFlannelVersion" ]
+    then
+      echo "$(gettext 'Upgrads flannel from') $currentFlannelVersion $(gettext 'to') $toFlannelVersion"
+      echo "$(gettext 'Remove rpm package cni-plugin-flannel-1.      kubectl -n kube-flannel delete daemonset.apps/kube-flannel-ds
+      kubectl apply -f http://sigstore.local:81/manifests/kube-flannel/0/25/1/kube-flannel.yml1.2')"
+      rpm -e --replacefiles --nodeps  cni-plugin-flannel-1.1.2
+      chown u7s-admin:u7s-admin -R /usr/libexec/cni/
+      rm -f /usr/libexec/cni/flannel
+
+      while :;
+      do
+        numberReady=$(kubectl  -n kube-flannel get daemonset.apps/kube-flannel-ds -o json | jq -r '.status.numberReady')
+        if [ "$numberReady" -gt 0 ] 2>/dev/null
+        then
+          break
+        fi
+        echo "$(gettext 'Waiting for service') kube-flannel:0.25.1 $(gettext 'to be restored')"
+        sleep 1
+      done
+    fi
+  fi
   echo "$(gettext 'Updating cluster node services to version') $kubeVersion"
   echo "$(gettext 'Wait several minutes...')"
   machinectl shell u7s-admin@ \
     /usr/libexec/podsec/u7s/bin/nsenter_u7s \
       /usr/bin/kubeadm upgrade  apply -y ${kubeVersion}
-  systemctl stop u7s
-  systemctl start u7s
+
+  echo -ne "$(gettext 'Cordon node, remove node pods and containers')"
+  kubectl cordon $HOSTNAME
+  kubectl drain $HOSTNAME --ignore-daemonsets --delete-emptydir-data
+  while killall kubelet 2>/dev/null; do sleep 1; done
+  TMPCMDFile="/tmp/cmd_$$.sh"
+  echo '
+while [ "$(crictl ps -qa | wc -l)" -ne 0 ]
+do
+  for p in $(crictl pods -q);
+  do
+    if [ "$(crictl inspectp $p | jq -r .status.linux.namespaces.options.network)" != "NODE" ];
+    then
+      crictl rmp -f $p;
+    fi;
+  done
+  crictl rmp -fa
+  sleep 1
+done
+' > $TMPCMDFile
+
+  machinectl shell u7s-admin@ /usr/libexec/podsec/u7s/bin/nsenter_u7s sh -x $TMPCMDFile
+# exit
+    systemctl stop u7s
+
+  listPrevRPMs=$(rpm -qa | grep 'cri-o
+cri-tools
+kubernetes')
+
+
+  echo "$(gettext 'Removing rpm packeges') $listPrevRPMs $(gettext 'of previous kubernetes version ') ${prevKubeMinorVersion}"
+  rpm -e --nodeps $listPrevRPMs
+
+  pushd $U7S_rpmDir
+#   pushd /var/sigstore/rpms/
+  listNewRPMS=$(echo kubernetes${kubeMinorVersion}-* \
+    cri-o${kubeMinorVersion}-* \
+    cri-tools${kubeMinorVersion}-*\
+    cni-plugin-flannel-1.*\
+    cni-plugins-* \
+    )
+  if [[ $curMinorKubeVersion -gt '1.29' ]]
+  then
+    listNewRPMS+=$(echo crun-*)
+  fi
+  echo "$(gettext 'Installing rpm packeges') $listNewRPMS $(gettext 'of new kubernetes version ') ${kubeMinorVersion}"
+  apt-get install -y $listNewRPMS
+#   for pkg in cri-o${kubeMinorVersion}-* cri-tools${kubeMinorVersion}-*
+#   do
+#     echo "$(gettext 'Installing') $pkg $(gettext 'for next kubernetes version ') $kubeVersion"
+#     rpm -i --replacefiles --nodeps $pkg
+#   done
+  popd
 
   echo -ne "$(gettext 'Restart node services')"
-  echo -ne "$(gettext 'Waiting for kubelet node services to come up') ."
   sleep 1
-  while :;
-  do
-    nodeCurrentKubeletVersion=$(getNodeCurrentKubeletVersion)
-    nodeCurrentKubeletVersion=$(getMinorVersion $nodeCurrentKubeletVersion)
-    if [ -n "$nodeCurrentKubeletVersion" ]
-    then
-      if [ "$nodeCurrentKubeletVersion" != "${kubeMinorVersion}" ]
-      then
-        echo -ne "\n$(gettext 'The kubelet version') $nodeCurrentKubeletVersion $(gettext 'on the node') $U7S_HOSTNAME $(gettext 'does not match the target version') $kubeMinorVersion\n" >&2
-      else
-        break
-      fi
-    fi
-    echo -ne .
-    sleep 1
-  done
+  systemctl start u7s
+  machinectl shell u7s-admin@ /usr/bin/systemctl --user start kubelet
 
   echo -ne "$(gettext 'Waiting for kubeapi node services to come up') ."
   sleep 1
@@ -269,26 +363,28 @@ do
     sleep 1
   done
 
-  pushd $U7S_rpmDir
-  for pkg in kubernetes${kubeMinorVersion}-* cri-o${kubeMinorVersion}-*
+  echo -ne "$(gettext 'Waiting for kubelet node services to come up') ."
+  while :;
   do
-    echo "$(gettext 'Installing') $pkg $(gettext 'for next kubernetes version ') $kubeVersion"
-    rpm -i --replacefiles --nodeps $pkg
+    nodeCurrentKubeletVersion=$(getNodeCurrentKubeletVersion)
+    nodeCurrentKubeletVersion=$(getMinorVersion $nodeCurrentKubeletVersion)
+    if [ -n "$nodeCurrentKubeletVersion" ]
+    then
+      if [ "$nodeCurrentKubeletVersion" != "${kubeMinorVersion}" ]
+      then
+        echo -ne "\n$(gettext 'The kubelet version') $nodeCurrentKubeletVersion $(gettext 'on the node') $U7S_HOSTNAME $(gettext 'does not match the target version') $kubeMinorVersion\n" >&2
+      else
+        break
+      fi
+    fi
+    echo -ne .
+    sleep 1
   done
-  popd
-  listPrevRPMs=$(rpm -qa | grep 'cri-o
-kubernetes' | grep 'cri-o
-master
-client
-node
-common
-crio
-kubelet
-kubeadm' | grep ${prevKubeMinorVersion})
-  echo "$(gettext 'Removing rpm packeges') $listPrevRPMs $(gettext 'of previous kubernetes version ') ${prevKubeMinorVersion}"
-  rpm -e --nodeps $listPrevRPMs
-  machinectl shell u7s-admin@ /usr/bin/crictl rmi $prevImages
-  prevImages=$(machinectl shell u7s-admin@ /usr/bin/kubeadm config images list --image-repository=$U7S_REGISTRY 2>/dev/null)
+
+  kubectl uncordon $HOSTNAME
+
+#   prevImages=$(machinectl shell u7s-admin@ /usr/bin/kubeadm config images list --image-repository=$U7S_REGISTRY 2>/dev/null)
+#   machinectl shell u7s-admin@ /usr/bin/crictl rmi $prevImages
   if [ -z "$(getCurrentKubeAPIVersion)" ]
   then
     echo "$(gettext 'kubernetes services down')"
